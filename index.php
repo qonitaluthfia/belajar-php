57194db7b60f68a3b617d6b1c395af0e67b1d1aa
qonitaluthfia <qonitaluthfiaalimah@gmail.com>
2022-10-24 10:55:48 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+echo "Hello World! <br>";
+
+$nama = "Qonita";
+$umur = 22;
+
+echo "Nama Saya <strong> $nama </strong>, Saya berusia $umur tahun. <br>";
+
+
+$namaadik = "Rijal";
+$umuradik = 18;
+
+echo "Nama adik saya <strong> $namaadik </strong>, adik saya berusia $umuradik tahun.<br>";
+
+$selisihumur = $umur - $umuradik;
+
+echo "Selisih usia saya dan adik saya adalah $selisihumur tahun.";
+
+?>
